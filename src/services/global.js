1273f282ed05f2a71ce697278ebc0ffe15a3a1f6
Rapiwash <sistemarapiwash@gmail.com>
2024-05-14 18:33:47 -0500

    add project

---
+++ src/services/global.js
@@ -0,0 +1,37 @@
+// Zona Horaria
+export const timeZone = "America/Guatemala";
+// Tipo de Moneda
+export const tipoMoneda = "GTQ";
+// Formato de la moneda (separacion de miles - decimales) segun la configuracion regional
+export const confMoneda = "es-GT";
+export const simboloMoneda = "Q";
+export const nameMoneda = "Quetzales";
+export const ingresoDigital = "TRANSFERENCIA MOVIL";
+
+export const politicaAbandono = {
+  mResaltado:
+    "El plazo maximo para retirar las prendas es de 8 días, de lo contrario no nos responsabilizamos de las mismas",
+  mGeneral: `; No hay lugar a reclamo una ves retirada la prenda No nos reposabilizamos por prendas que se destiñan por malos tintes, botones o adornos 
+   que no resistan al lavado o planchado, por las prendas que se deterioren por estar demasiado usadas, tejidos y confecciones defectuosas.`,
+};
+
+export const documento = "NIT"; // DNI
+
+export const nameImpuesto = "IVA"; // IGV
+
+export const codigoPhonePais = "502";
+
+export const MONTOS_BASE = [
+  { monto: 200, cantidad: "", total: 0 },
+  { monto: 100, cantidad: "", total: 0 },
+  { monto: 50, cantidad: "", total: 0 },
+  { monto: 20, cantidad: "", total: 0 },
+  { monto: 10, cantidad: "", total: 0 },
+  { monto: 5, cantidad: "", total: 0 },
+  { monto: 1, cantidad: "", total: 0 },
+  { monto: 0.5, cantidad: "", total: 0 },
+  { monto: 0.25, cantidad: "", total: 0 },
+  { monto: 0.1, cantidad: "", total: 0 },
+  { monto: 0.05, cantidad: "", total: 0 },
+  { monto: 0.01, cantidad: "", total: 0 },
+];
